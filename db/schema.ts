b22fe074363e98e64dcbe0b09bea91e6e52c0a20
whit33y <troppout@gmail.com>
2026-02-07 14:47:55 +0100

    chore: Add db schemas

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,30 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { index, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
+
+export const snippetsTable = pgTable(
+  "snippets",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer()
+      .references(() => usersTable.id, { onDelete: "cascade" })
+      .notNull(),
+    title: varchar({ length: 255 }).notNull(),
+    code: text().notNull(),
+    language: varchar({ length: 255 }).notNull(),
+    framework: varchar({ length: 255 }),
+    description: text(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp().defaultNow().notNull(),
+  },
+  (table) => [
+    index("language_idx").on(table.language),
+    index("framework_idx").on(table.framework),
+  ]
+);
