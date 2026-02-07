f206febe3f081e91b4040eda84f1818f732ed6d3
Oskar Puchalski <oskarpuchalski17@gmail.com>
2026-02-07 22:51:46 +0100

    feat: Add simple snippet details view

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,4 +1,8 @@
 import { db } from '@/db'
 import { snippetsTable } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 
 export const getSnippets = async () => db.select().from(snippetsTable)
+
+export const getSnippetById = async (id: number) =>
+  db.select().from(snippetsTable).where(eq(snippetsTable.id, id))
