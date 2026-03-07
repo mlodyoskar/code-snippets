8fabf7b16407100c5a05273c804b001da6e2aba8
whit33y <troppout@gmail.com>
2026-03-07 17:16:33 +0100

    feat: add stats-cards component with data

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,20 @@
-import Onboard from "@/components/onboard";
-import { getRecentSnippets, getSnippetStats } from "@/lib/queries";
+import Onboard from '@/components/onboard'
+import StatsCards from '@/components/stats-cards'
+import { getRecentSnippets, getSnippetStats } from '@/lib/queries'
 
-export default async function Page(){
-  const recent = await getRecentSnippets();
-  const stats = await getSnippetStats();
+export default async function Page() {
+  const recent = await getRecentSnippets()
+  const stats = await getSnippetStats()
   return (
     <div className="mx-auto w-full max-w-5xl p-4 md:p-6 lg:p-8">
       <Onboard />
+      <section className="border-input mt-3 flex flex-col rounded-xl border-2 p-6">
+        <StatsCards
+          total={stats.total}
+          topLanguage={stats.topLanguage}
+          topFramework={stats.topFramework}
+        />
+      </section>
     </div>
-  );
+  )
 }
-
-
