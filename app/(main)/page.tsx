0f18919cb0b58770ef6676f34c41ea7f620e70cd
whit33y <troppout@gmail.com>
2026-03-07 16:49:02 +0100

    feat: add queries for last 6 and top language/framework

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,9 @@
 import Onboard from "@/components/onboard";
+import { getRecentSnippets, getSnippetStats } from "@/lib/queries";
 
-const Page = () => {
+export default async function Page(){
+  const recent = await getRecentSnippets();
+  const stats = await getSnippetStats();
   return (
     <div className="mx-auto w-full max-w-5xl p-4 md:p-6 lg:p-8">
       <Onboard />
@@ -9,4 +12,3 @@ const Page = () => {
 }
 
 
-export default Page
