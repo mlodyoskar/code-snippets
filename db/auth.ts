00a9dcd2069b5157c79d81afa1273214adfded8a
Jakub Bryska <troppout@gmail.com>
2026-03-13 13:18:26 +0100

    feat: add libs and basic configuration for authentication

---
+++ db/auth.ts
@@ -0,0 +1,20 @@
+import type { NextAuthOptions } from 'next-auth'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '.'
+import Google from 'next-auth/providers/google'
+import { accounts, sessions, usersTable, verificationTokens } from './schema'
+
+export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db, {
+    usersTable: usersTable,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+}
